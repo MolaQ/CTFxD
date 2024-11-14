b6216d78915b9ecbb16ad94443ab921530737168
MolaQ <marcin.molak@gmail.com>
2024-11-14 21:24:36 +0100

    remove style tester | navbar toggler

--- resources/views/livewire/components/navbar.blade.php
+++ resources/views/livewire/components/navbar.blade.php
@@ -1,25 +1,35 @@
 <div>
-
-    <nav class="navbar sticky-top navbar-dark" style="background-color: #780000;">
+    <nav class="navbar navbar-expand-lg bg-rdm">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
+            <a class="navbar-brand text-white" href="{{ route('home') }}">{{ config('app.name') }}</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
+                aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a href="/" class="btn btn-orange {{ request()->is('/') ? 'active' : '' }}" wire:navigate >Home</a>
+                    </li>
 
-                @guest
-                <div>
-                    <a href="{{ route('login') }}" class="btn btn-light mr-1 {{ request()->is('login') ? 'active' : '' }} ">
-                        Login</a>
-                    <a href="{{ route('register') }}" class="btn btn-light mr-1 {{ request()->is('register') ? 'active' : '' }}">
-                        Register</a>
+                </ul>
+                <div class="d-flex">
+                    @guest
+                    <div>
+                        <a href="{{ route('login') }}" class="btn btn-light mr-1 {{ request()->is('login') ? 'active' : '' }} ">
+                            Login</a>
+                        <a href="{{ route('register') }}" class="btn btn-light mr-1 {{ request()->is('register') ? 'active' : '' }}">
+                            Register</a>
+                    </div>
+
+                    @endguest
+                    @auth
+
+                    @livewire('components.logout')
+                    @endauth
                 </div>
-
-                @endguest
-                @auth
-
-                @livewire('components.logout')
-                @endauth
             </div>
         </div>
-
     </nav>
-
 </div>
+
--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,35 +1,6 @@
 <div>
     <h1>Close your eyes. Count to one. That is how long forever feels.</h1>
 
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-purple" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        MY CUSTOM COLOR
-    </button>
-
-    <div class="alert bg-pink" role="alert">
-        <p class="text-cyan-900">A simple primary alert—check it out!</p>
-    </div>
-    <div class="alert bg-orange text-purple" role="alert">
-        A simple secondary alert—check it out!
-    </div>
-    <div class="alert alert-success" role="alert">
-        A simple success alert—check it out!
-    </div>
-    <div class="alert alert-danger" role="alert">
-        A simple danger alert—check it out!
-    </div>
-    <div class="alert alert-warning" role="alert">
-        A simple warning alert—check it out!
-    </div>
-    <div class="alert alert-info" role="alert">
-        A simple info alert—check it out!
-    </div>
-    <div class="alert alert-light" role="alert">
-        A simple light alert—check it out!
-    </div>
-    <div class="alert alert-dark" role="alert">
-        A simple dark alert—check it out!
-    </div>
     @auth
         Zalogowany!
         @if (Auth::user()->isAdmin())
