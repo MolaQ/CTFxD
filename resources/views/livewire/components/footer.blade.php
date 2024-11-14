2f2a20fd920606f84840cc9eddacc159a791e114
MolaQ <marcin.molak@gmail.com>
2024-11-14 21:26:39 +0100

    footer new sass color rdm

--- resources/views/livewire/components/footer.blade.php
+++ resources/views/livewire/components/footer.blade.php
@@ -1,6 +1,6 @@
 <div>
 
-    <nav class="navbar fixed-bottom navbar-dark text-light" style="background-color: #780000;">
+    <nav class="navbar fixed-bottom bg-rdm text-light">
         <div class="container-fluid">
             <p>Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
             <p>This page took {{ round(microtime(true) - LARAVEL_START, 3) }} seconds to render</p>
