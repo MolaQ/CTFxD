8d26d9b2a3aa8d9f973c083c9edc34f967b4b0d9
MolaQ <marcin.molak@gmail.com>
2024-11-27 13:47:34 +0100

    cosmetic

--- resources/views/livewire/admin/layouts/components/flash.blade.php
+++ resources/views/livewire/admin/layouts/components/flash.blade.php
@@ -1,11 +1,11 @@
 @if (session()->has('success'))
-<div class="flash-message alert alert-success rounded mx-2">
-    <strong>Yep!</strong> {{ session('success') }}
-</div>
+    <div class="flash-message alert alert-success rounded mx-2 my-1">
+        <strong>Yep!</strong> {{ session('success') }}
+    </div>
 @endif
 
 @if (session()->has('danger'))
-<div class="flash-message alert alert-danger rounded mx-2">
-    <strong>Whooops!</strong> {{ session('danger') }}
-</div>
+    <div class="flash-message alert alert-danger rounded mx-2 my-1">
+        <strong>Whooops!</strong> {{ session('danger') }}
+    </div>
 @endif
