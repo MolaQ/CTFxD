ae513d16d57af7ab7dd5651bd75a01df83d10cf2
MolaQ <marcin.molak@gmail.com>
2024-11-29 10:16:00 +0100

    admin teams blade table

--- resources/views/livewire/admin/admin-teams.blade.php
+++ resources/views/livewire/admin/admin-teams.blade.php
@@ -23,7 +23,79 @@
                             <div class="col-12"> <!-- Default box -->
                                 {{-- Flash messages --}}
                                 @include('livewire.admin.layouts.components.flash')
-                                {{-- end Flash messages --}}
+
+                                <div class="card">
+                                    <div class="card-header">
+                                        <div class="mb-3">
+                                            <label for="search" class="form-label">Szukaj Zespołu</label>
+                                            <div class="row">
+                                                <!-- Kolumna 1: Pole wyszukiwania -->
+                                                <div class="col-3">
+                                                    <input wire:model.live="search" type="email" id="search"
+                                                        class="form-control" placeholder="Szukaj">
+                                                </div>
+
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="card mb-4">
+                                            <div class="card-header">
+                                                <h3 class="card-title pt-2">Teams list
+                                                </h3>
+                                            </div> <!-- /.card-header -->
+                                            <div class="card-body p-0">
+                                                <table class="table table-sm">
+                                                    <thead>
+                                                        <tr>
+                                                            <th style="width: 2%">#</th>
+                                                            <th>Team name</th>
+                                                            <th>Members</th>
+                                                            <th style="width: 15%">Action</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        @foreach ($allTeams as $t)
+                                                            <tr class="align-middle">
+                                                                <td>{{ $loop->iteration }}</td>
+                                                                <td>{{ $t->name }}</td>
+                                                                <td></td>
+                                                                <td>
+                                                                    <a wire:click="modify({{ $t->id }})"
+                                                                        class="btn rounded btn-primary"><i
+                                                                            class="nav-icon bi bi-building-gear"></i></a>
+                                                                    <a onclick="return confirm('Are you sure you want to delete this item?') || event.stopImmediatePropagation()"
+                                                                        wire:click="delete({{ $t->id }})""
+                                                                        class="btn rounded btn-danger"><i
+                                                                            class="nav-icon bi bi-trash"></i></a>
+                                                                </td>
+                                                            </tr>
+                                                        @endforeach
+
+                                                    </tbody>
+                                                </table>
+                                                <div class="mx-5 py-2">{{ $allTeams->links() }}</div>
+                                            </div> <!-- /.card-body -->
+                                        </div>
+                                    </div> <!-- /.card-body -->
+                                    <div class="card-footer">
+                                        <div class="row">
+
+                                            <!-- Kolumna 2: Checkbox "Niekatywni" -->
+
+                                            <div class="col-2">
+                                                <p>filters...</p>
+
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                    <!-- /.card-footer-->
+                                </div> <!-- /.card -->
+
+
                             </div>
                         </div> <!--end::Row-->
                     </div>
