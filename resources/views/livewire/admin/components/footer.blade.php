0f465ebde0660c8a6b00d938eb4c920cbac7168b
MolaQ <marcin.molak@gmail.com>
2024-11-30 20:46:09 +0100

    Inspire quote in footer

--- resources/views/livewire/admin/components/footer.blade.php
+++ resources/views/livewire/admin/components/footer.blade.php
@@ -1,11 +1,5 @@
-<div>
+
     <footer class="app-footer"> <!--begin::To the end-->
         <div class="float-end d-none d-sm-inline">{{ $message }}</div> <!--end::To the end-->
-        <!--begin::Copyright--> <strong>
-            Copyright &copy; 2014-2024&nbsp;
-            <a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>.
-        </strong>
-        All rights reserved.
-        <!--end::Copyright-->
+        <p>This page took {{ round(microtime(true) - LARAVEL_START, 3) }} seconds to render</p>
     </footer> <!--end::Footer-->
-</div>
