040f47364a6adac72a74421cc3d0e06bd1bbe841
MolaQ <marcin.molak@gmail.com>
2024-12-12 16:02:35 +0100

    trash removed, alert instead flash

--- app/Livewire/Admin/Components/Logout.php
+++ app/Livewire/Admin/Components/Logout.php
@@ -3,14 +3,16 @@
 namespace App\Livewire\Admin\Components;
 
 use Illuminate\Support\Facades\Auth;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
 
 class Logout extends Component
 {
+    use LivewireAlert;
     public function logout()
     {
         Auth::logout();
-        session()->flash('success', 'Wylogowanie pomyślne, do zobaczenia później');
+        $this->alert('info', 'Wylogowanie pomyślne, do zobaczenia później');
 
         return redirect('/');
     }
