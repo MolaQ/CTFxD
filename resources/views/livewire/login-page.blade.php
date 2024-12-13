c2603f9c9e3d250e8224a53f684da807ab413681
MolaQ <marcin.molak@gmail.com>
2024-12-13 15:16:22 +0100

    styled

--- resources/views/livewire/login-page.blade.php
+++ resources/views/livewire/login-page.blade.php
@@ -1,38 +1,34 @@
-<div class="container my-1">
-    <div class="justify-content-center">
-        <div class="col-md-6 offset-md-3">
-            <div class="card">
+<div class="col-md-6 offset-md-3">
+    <div class="card">
 
-                <div class="card-header bg-rdm text-white">
-                    <h2>{{ __('Sign in!') }}</h2>
+        <div class="card-header bg-rdm text-white">
+            <h2>{{ __('Sign in!') }}</h2>
+        </div>
+        <div class="card-body">
+            <form wire:submit.prevent="authenticate">
+                @csrf
+                <div class="form-group">
+                    <label for="email">{{ __('email') }}</label>
+                    <input type="email" class="form-control @error('email') is-invalide @enderror" id="email"
+                        wire:model="email">
                 </div>
-                <div class="card-body">
-                    <form wire:submit.prevent="authenticate">
-                        @csrf
-                        <div class="form-group">
-                            <label for="email">{{ __('email') }}</label>
-                            <input type="email" class="form-control @error('email') is-invalide @enderror"
-                                id="email" wire:model="email">
-                        </div>
-                        @error('email')
-                            <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                        <div class="form-group">
-                            <label for="password">{{ __('password') }}</label>
-                            <input type="password" class="form-control @error('password') is-invalide @enderror"
-                                id="email" wire:model="password">
-                            @error('password')
-                                <p class="text-danger">{{ $message }}</p>
-                            @enderror
+                @error('email')
+                    <p class="text-danger">{{ $message }}</p>
+                @enderror
+                <div class="form-group">
+                    <label for="password">{{ __('password') }}</label>
+                    <input type="password" class="form-control @error('password') is-invalide @enderror" id="email"
+                        wire:model="password">
+                    @error('password')
+                        <p class="text-danger">{{ $message }}</p>
+                    @enderror
 
 
-                        </div>
-                        <div class="d-grid gap-2">
-                            <button type="submit" class="btn bg-rdm btn-lg text-white mt-5">Login</button>
-                        </div>
-                    </form>
                 </div>
-            </div>
+                <div class="d-grid gap-2">
+                    <button type="submit" class="btn bg-rdm btn-lg text-white mt-5">Login</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
--- resources/views/livewire/register-page.blade.php
+++ resources/views/livewire/register-page.blade.php
@@ -1,52 +1,47 @@
-<div class="container my-1">
-    <div class="justify-content-center">
-        <div class="col-md-6 offset-md-3">
-            <div class="card">
-                <div class="card-header bg-rdm text-white">
-                    <h2>{{ __('Join the game!') }}</h2>
+<div class="col-md-6 offset-md-3">
+    <div class="card">
+        <div class="card-header bg-rdm text-white">
+            <h2>{{ __('Join the game!') }}</h2>
+        </div>
+        <div class="card-body">
+            <form wire:submit.prevent="registration">
+                @csrf
+                <div class="form-group">
+                    <label for="name">{{ __('name') }}</label>
+                    <input type="text" class="form-control @error('name') is-invalide @enderror" id="name"
+                        wire:model="name">
+                    @error('name')
+                        <span clan="invalid-feedback">{{ $message }}</span>
+                    @enderror
                 </div>
-                <div class="card-body">
-                    <form wire:submit.prevent="registration">
-                        @csrf
-                        <div class="form-group">
-                            <label for="name">{{ __('name') }}</label>
-                            <input type="text" class="form-control @error('name') is-invalide @enderror"
-                                id="name" wire:model="name">
-                            @error('name')
-                                <span clan="invalid-feedback">{{ $message }}</span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="email">{{ __('email') }}</label>
-                            <input type="email" class="form-control @error('email') is-invalide @enderror"
-                                id="email" wire:model="email">
-                            @error('email')
-                                <span clan="invalid-feedback">{{ $message }}</span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="password">{{ __('password') }}</label>
-                            <input type="password" class="form-control @error('password') is-invalide @enderror"
-                                id="email" wire:model="password">
-                            @error('password')
-                                <span clan="invalid-feedback">{{ $message }}</span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="password_confirmation">{{ __('password confirmation') }}</label>
-                            <input type="password"
-                                class="form-control @error('password_confirmation') is-invalide @enderror"
-                                id="email" wire:model="password_confirmation" name="password">
-                            @error('password_confirmation')
-                                <span clan="invalid-feedback">{{ $message }}</span>
-                            @enderror
-                        </div>
-                        <div class="d-grid gap-2">
-                            <button type="submit" class="btn bg-rdm text-white btn-lg mt-5">Register</button>
-                        </div>
-                    </form>
+                <div class="form-group">
+                    <label for="email">{{ __('email') }}</label>
+                    <input type="email" class="form-control @error('email') is-invalide @enderror" id="email"
+                        wire:model="email">
+                    @error('email')
+                        <span clan="invalid-feedback">{{ $message }}</span>
+                    @enderror
                 </div>
-            </div>
+                <div class="form-group">
+                    <label for="password">{{ __('password') }}</label>
+                    <input type="password" class="form-control @error('password') is-invalide @enderror" id="email"
+                        wire:model="password">
+                    @error('password')
+                        <span clan="invalid-feedback">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="password_confirmation">{{ __('password confirmation') }}</label>
+                    <input type="password" class="form-control @error('password_confirmation') is-invalide @enderror"
+                        id="email" wire:model="password_confirmation" name="password">
+                    @error('password_confirmation')
+                        <span clan="invalid-feedback">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="d-grid gap-2">
+                    <button type="submit" class="btn bg-rdm text-white btn-lg mt-5">Register</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
