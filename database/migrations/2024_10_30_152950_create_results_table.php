a6641d945e8c5b31165c324cd852109ee7f34ae7
MolaQ <marcin.molak@gmail.com>
2024-12-16 09:20:21 +0100

    fix comments

--- database/migrations/2024_10_30_152950_create_results_table.php
+++ database/migrations/2024_10_30_152950_create_results_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignId('task_id')->constrained()->onDelete('cascade'); // Powiązanie z zadaniem
             $table->string('response'); // Odpowiedź użytkownika
             $table->boolean('is_correct')->default(false); // Czy odpowiedź jest poprawna
-            $table->float('points', 4, 3)->default(0); // Czy odpowiedź jest poprawna
+            $table->float('points', 4, 3)->default(0); // punkty
             $table->timestamps();
         });
     }
