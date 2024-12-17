aa21e43dae018a8fa67cc7a9207782513207a210
MolaQ <marcin.molak@gmail.com>
2024-12-17 11:16:10 +0100

    Faq on main page, in order from admin panel with filters

--- app/Livewire/FaqPage.php
+++ app/Livewire/FaqPage.php
@@ -2,12 +2,29 @@
 
 namespace App\Livewire;
 
+use App\Models\Faq;
 use Livewire\Component;
 
 class FaqPage extends Component
 {
+    public $search;
     public function render()
     {
-        return view('livewire.faq-page');
+        $faqsQuery = Faq::query();
+        //POBRANIE WG KOLEJNOSCI
+
+
+        // Filtrowanie po wyszukiwaniu (name, email, school name)
+        if (!empty($this->search)) {
+            $faqsQuery->where('name', 'like', '%' . $this->search . '%')
+                ->orWhere('description', 'like', '%' . $this->search . '%');
+        }
+        //$faqsQuery->orderBy('order', 'ASC');
+        $allFaqs = $faqsQuery->orderBy('order', 'ASC')->get();
+
+        //$allFaqs = Faq::orderBy('order', 'ASC')->get();
+        return view('livewire.faq-page', [
+            'allFaqs' => $allFaqs,
+        ]);
     }
 }
--- resources/views/livewire/faq-page.blade.php
+++ resources/views/livewire/faq-page.blade.php
@@ -1,66 +1,54 @@
 <div class="card">
     <div class="card-header bg-rdm text-white">
-        <h2>FAQ</h2>
+        <div class="d-flex justify-content-between mb-1 pt-1">
+            <!-- Lewa strona: Przycisk -->
+            <h3>FAQ</h3>
+
+            <!-- Prawa strona: Pole wyszukiwania -->
+            <div class="col-3">
+                <input wire:model.live="search" id="search" class="mt-1 form-control border-rdm shadow-none"
+                    placeholder="Szukaj...">
+            </div>
+        </div>
     </div>
     <div class="card-body">
         <div class="accordion" id="accordionPanelsStayOpenExample">
-            <div class="accordion-item">
-                <h2 class="accordion-header">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
-                        aria-controls="panelsStayOpen-collapseOne">
-                        Accordion Item #1
-                    </button>
-                </h2>
-                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
-                    <div class="accordion-body">
-                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the
-                        collapse plugin adds the appropriate classes that we use to style each element. These classes
-                        control the overall appearance, as well as the showing and hiding via CSS transitions. You can
-                        modify any of this with custom CSS or overriding our default variables. It's also worth noting
-                        that just about any HTML can go within the <code>.accordion-body</code>, though the transition
-                        does limit overflow.
+
+            @foreach ($allFaqs as $f)
+                @if ($loop->first)
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#{{ $f->id }}" aria-expanded="true"
+                                aria-controls="panelsStayOpen-collapseOne">
+                                {{ $f->name }}
+                            </button>
+                        </h2>
+                        <div id="{{ $f->id }}" class="accordion-collapse collapse show">
+                            <div class="accordion-body">
+                                {{ $f->description }}
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false"
-                        aria-controls="panelsStayOpen-collapseTwo">
-                        Accordion Item #2
-                    </button>
-                </h2>
-                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
-                    <div class="accordion-body">
-                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the
-                        collapse plugin adds the appropriate classes that we use to style each element. These classes
-                        control the overall appearance, as well as the showing and hiding via CSS transitions. You can
-                        modify any of this with custom CSS or overriding our default variables. It's also worth noting
-                        that just about any HTML can go within the <code>.accordion-body</code>, though the transition
-                        does limit overflow.
+                @endif
+                @if (!$loop->first)
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#{{ $f->id }}" aria-expanded="false"
+                                aria-controls="panelsStayOpen-collapseTwo">
+                                {{ $f->name }}
+                            </button>
+                        </h2>
+                        <div id="{{ $f->id }}" class="accordion-collapse collapse">
+                            <div class="accordion-body">
+                                {{ $f->description }}
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false"
-                        aria-controls="panelsStayOpen-collapseThree">
-                        Accordion Item #3
-                    </button>
-                </h2>
-                <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
-                    <div class="accordion-body">
-                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the
-                        collapse plugin adds the appropriate classes that we use to style each element. These classes
-                        control the overall appearance, as well as the showing and hiding via CSS transitions. You can
-                        modify any of this with custom CSS or overriding our default variables. It's also worth noting
-                        that just about any HTML can go within the <code>.accordion-body</code>, though the transition
-                        does limit overflow.
-                    </div>
-                </div>
-            </div>
+                @endif
+            @endforeach
+
         </div>
     </div>
     <div class="card-footer">
