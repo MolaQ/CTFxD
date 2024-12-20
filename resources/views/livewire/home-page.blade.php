93b4ce2f7b965900bed9f6adb1c32166b74d3c23
MolaQ <marcin.molak@gmail.com>
2024-12-20 15:12:34 +0100

    responsive

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-lg-4 col-sm-6">
+    <div class="col-lg-3 col-md-4 col-sm-6">
         <div class="card text-center border-0 shadow rounded-5 p-3 my-3" style="max-width: 22rem;">
             <div class="icon text-rdm" style="font-size: 3rem;">
                 <i class="bi bi-bullseye"></i>
@@ -13,7 +13,7 @@
             </div>
         </div>
     </div>
-    <div class="col-lg-4 col-sm-6">
+    <div class="col-lg-3 col-md-4 col-sm-6">
         <div class="card text-center border-0 shadow rounded-5 p-3 my-3" style="max-width: 22rem;">
             <div class="icon text-rdm" style="font-size: 3rem;">
                 <i class="bi bi-bullseye"></i>
@@ -27,7 +27,21 @@
             </div>
         </div>
     </div>
-    <div class="col-lg-4 col-sm-6">
+    <div class="col-lg-3 col-md-4 col-sm-6">
+        <div class="card text-center border-0 shadow rounded-5 p-3 my-3" style="max-width: 22rem;">
+            <div class="icon text-rdm" style="font-size: 3rem;">
+                <i class="bi bi-bullseye"></i>
+            </div>
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's
+                    content.</p>
+                <a href="#" class="btn btn-rdm text-white">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6">
         <div class="card text-center border-0 shadow rounded-5 p-3 my-3" style="max-width: 22rem;">
             <div class="icon text-rdm" style="font-size: 3rem;">
                 <i class="bi bi-bullseye"></i>
