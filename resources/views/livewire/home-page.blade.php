ee72569a217f8198d2924f996ef41a6f6c0d0b85
MolaQ <marcin.molak@gmail.com>
2024-12-20 15:06:37 +0100

    first scratch of cards...

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,16 +1,44 @@
-<div>
-    <h1>Close your eyes. Count to one. That is how long forever feels.</h1>
-
-    @auth
-        Zalogowany!
-        @if (Auth::user()->isAdmin())
-            <p>Witaj, Administratorze!</p>
-            <!-- Kod dla administratora -->
-        @else
-            <p>Witaj, Użytkowniku!</p>
-            <!-- Kod dla zwykłego użytkownika -->
-        @endif
-    @endauth
-
-
+<div class="row">
+    <div class="col-lg-4 col-sm-6">
+        <div class="card text-center border-0 shadow rounded-5 p-3 my-3" style="max-width: 22rem;">
+            <div class="icon text-rdm" style="font-size: 3rem;">
+                <i class="bi bi-bullseye"></i>
+            </div>
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's
+                    content.</p>
+                <a href="#" class="btn btn-rdm text-white">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4 col-sm-6">
+        <div class="card text-center border-0 shadow rounded-5 p-3 my-3" style="max-width: 22rem;">
+            <div class="icon text-rdm" style="font-size: 3rem;">
+                <i class="bi bi-bullseye"></i>
+            </div>
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's
+                    content.</p>
+                <a href="#" class="btn btn-rdm text-white">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4 col-sm-6">
+        <div class="card text-center border-0 shadow rounded-5 p-3 my-3" style="max-width: 22rem;">
+            <div class="icon text-rdm" style="font-size: 3rem;">
+                <i class="bi bi-bullseye"></i>
+            </div>
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's
+                    content.</p>
+                <a href="#" class="btn btn-rdm text-white">Go somewhere</a>
+            </div>
+        </div>
+    </div>
 </div>
